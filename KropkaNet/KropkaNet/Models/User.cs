6cf290a0cebe992e802a9dc40bb769dcfbec1cd8
grochochotowski <g.m.rochowski@gmail.com>
2024-03-04 16:21:26 +0100

    Create user model

---
+++ KropkaNet/KropkaNet/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KropkaNet.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Pesel { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+
+
+        public List<int> Orderds { get; set; }
+        public List<int> Companies { get; set; }
+    }
+}
