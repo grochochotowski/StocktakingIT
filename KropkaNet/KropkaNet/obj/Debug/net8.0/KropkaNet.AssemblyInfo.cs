38e05e342150e5ba063003c05110a5e0745a1015
grochochotowski <g.m.rochowski@gmail.com>
2024-03-04 16:47:26 +0100

    add sql server

--- KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
+++ KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f8bf9a1368929a86b592820cba2b0f2ae0399f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a381c7253089ccfac04dc59ec3e7b5485c8c9e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
