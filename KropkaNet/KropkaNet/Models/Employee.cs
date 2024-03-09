d0986dfe804b26c2e53e0c7cf124efb216695c79
grochochotowski <g.m.rochowski@gmail.com>
2024-03-10 00:20:21 +0100

    create employee model

--- KropkaNet/KropkaNet/Models/Employee.cs
+++ KropkaNet/KropkaNet/Models/Employee.cs
@@ -4,5 +4,23 @@ namespace KropkaNet.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string PersonalNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public int PositionId { get; set; }
+        public string? Note { get; set; }
+
+        [Required]
+        public virtual Position Position { get; set; }
     }
 }
