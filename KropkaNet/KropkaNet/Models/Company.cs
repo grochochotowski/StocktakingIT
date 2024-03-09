cb7625fe4dd42390e37182fd1a98777fd51aa6ee
karichil <karina.chilkiewicz@gmail.com>
2024-03-10 00:20:13 +0100

    add company attributes

--- KropkaNet/KropkaNet/Models/Company.cs
+++ KropkaNet/KropkaNet/Models/Company.cs
@@ -1,6 +1,19 @@
-﻿namespace KropkaNet.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KropkaNet.Models;
 
 public class Company
 {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public int NIP { get; set; }
+    [Required]
+    public int KRS { get; set; }
+    [Required]
+    public string NazwaFirmy { get; set; }
     
+    //public virtual ICollection<> { get; set; }
+    //public virtual ICollection<> { get; set; }
+
 }
