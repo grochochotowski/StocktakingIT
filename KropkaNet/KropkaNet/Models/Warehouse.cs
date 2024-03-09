3950e935d390e2a01133f7f570d6eb10ad124ff3
grochochotowski <g.m.rochowski@gmail.com>
2024-03-10 00:26:23 +0100

    create warehouse model

--- KropkaNet/KropkaNet/Models/Warehouse.cs
+++ KropkaNet/KropkaNet/Models/Warehouse.cs
@@ -4,5 +4,13 @@ namespace KropkaNet.Models
 {
     public class Warehouse
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
     }
 }
