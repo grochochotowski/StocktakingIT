5dc900adb6f1c261622f216378ec36e1864bac47
grochochotowski <g.m.rochowski@gmail.com>
2024-03-10 00:23:55 +0100

    create product model

--- KropkaNet/KropkaNet/Models/Product.cs
+++ KropkaNet/KropkaNet/Models/Product.cs
@@ -4,5 +4,12 @@ namespace KropkaNet.Models
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Note { get; set; }
     }
 }
