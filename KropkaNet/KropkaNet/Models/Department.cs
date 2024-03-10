2b4f6589c15e094f762ddf23de9910da0db5bbe2
graccz103 <graccz103@gmail.com>
2024-03-10 18:27:52 +0100

    add error message

--- KropkaNet/KropkaNet/Models/Department.cs
+++ KropkaNet/KropkaNet/Models/Department.cs
@@ -6,6 +6,6 @@ public class Department
 {
     [Key]
     public int Id { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Name of the Department is required")]
     public string DepartmentName { get; set; }
 }
