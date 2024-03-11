22ffaf4cfc39eb5abc33964b616f10af9b241e13
grochochotowski <g.m.rochowski@gmail.com>
2024-03-11 16:32:20 +0100

    add db context service to program

--- KropkaNet/KropkaNet/Program.cs
+++ KropkaNet/KropkaNet/Program.cs
@@ -1,3 +1,7 @@
+using KropkaNet.Models.system;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace KropkaNet
 {
     public class Program
@@ -8,6 +12,8 @@ namespace KropkaNet
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<StocktakingContext>(o => o.UseSqlServer(configuration.GetConnectionString("ShopDbConnection")));
 
             var app = builder.Build();
 
