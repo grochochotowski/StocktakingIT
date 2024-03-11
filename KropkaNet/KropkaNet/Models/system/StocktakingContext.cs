31f987d010728d3155b2ececd923abff3d3179d3
grochochotowski <g.m.rochowski@gmail.com>
2024-03-11 16:07:49 +0100

    add ctor for db context

--- KropkaNet/KropkaNet/Models/system/StocktakingContext.cs
+++ KropkaNet/KropkaNet/Models/system/StocktakingContext.cs
@@ -1,6 +1,9 @@
-﻿namespace KropkaNet.Models.system
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KropkaNet.Models.system
 {
-    public class StocktakingContext
+    public class StocktakingContext : DbContext
     {
+        public StocktakingContext(DbContextOptions options) : base(options) { }
     }
 }
