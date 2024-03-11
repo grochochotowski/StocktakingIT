3e2ff8a4d2a1bc0c01a64d1ae9f9a2e4915b9c41
grochochotowski <g.m.rochowski@gmail.com>
2024-03-11 16:20:48 +0100

    add db sets to context

--- KropkaNet/KropkaNet/Models/system/StocktakingContext.cs
+++ KropkaNet/KropkaNet/Models/system/StocktakingContext.cs
@@ -6,7 +6,19 @@ namespace KropkaNet.Models.system
     public class StocktakingContext : DbContext
     {
         public StocktakingContext(DbContextOptions options) : base(options) { }
-    }
 
-    
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stocktaking> Stocktakings { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+
+
+
+    }
 }
