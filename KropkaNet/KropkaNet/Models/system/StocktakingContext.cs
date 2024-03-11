110742623588d0e7155f3af69656a5ce65c9479a
grochochotowski <g.m.rochowski@gmail.com>
2024-03-11 17:14:20 +0100

    create db sets for context

--- KropkaNet/KropkaNet/Models/system/StocktakingContext.cs
+++ KropkaNet/KropkaNet/Models/system/StocktakingContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using KropkaNet.Models.Objects;
+using KropkaNet.Models.Objects.ClientSide;
+using KropkaNet.Models.Objects.CompanySide;
 
 namespace KropkaNet.Models.system
 {
