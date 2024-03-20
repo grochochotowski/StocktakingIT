2606868746e7c8b160f64851e49c2311376ddd0e
karichil <karina.chilkiewicz@gmail.com>
2024-03-20 16:06:27 +0100

    create stocktaking controller

--- KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
+++ KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52472e7760943f62edb8b6a499b7f3ef129d46e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f619f0367be996322d6270c06d7febfe8a4eb98")]
 [assembly: System.Reflection.AssemblyProductAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
