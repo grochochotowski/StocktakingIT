252469366726a0c49646e77adfefd04f4d924f03
grochochotowski <g.m.rochowski@gmail.com>
2024-03-20 16:23:28 +0100

    auto

--- KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
+++ KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d544f5017ceadf08ab116d7c7b56f1cd536e6fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f880db6c74b0e463cf55f3cb6e247e6c195a443a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
