4fbcdc3214af35ef131141ea7b7072e87c0d04b7
grochochotowski <g.m.rochowski@gmail.com>
2024-03-20 14:22:48 +0100

    add quantity

--- KropkaNet/KropkaNet/Models/Objects/CompanySide/WarehouseProduct.cs
+++ KropkaNet/KropkaNet/Models/Objects/CompanySide/WarehouseProduct.cs
@@ -1,10 +1,22 @@
-﻿namespace KropkaNet.Models.Objects.CompanySide
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KropkaNet.Models.Objects.CompanySide
 {
     public class WarehouseProduct
     {
+        [Required(ErrorMessage = "Warehouse ID is required")]
         public int WarehouseId { get; set; }
         public virtual Warehouse Warehouse { get; set; }
+
+
+
+        [Required(ErrorMessage = "Product ID required")]
         public int ProductId { get; set; }
         public virtual Product Product { get; set; }
+
+
+
+        [Required(ErrorMessage = "Quantity is required")]
+        public int Quantity { get; set; }
     }
 }
