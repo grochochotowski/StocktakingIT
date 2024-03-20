ebd44a3d0a896154c8d44a8426275fb36dbf3f86
karichil <karina.chilkiewicz@gmail.com>
2024-03-20 16:19:01 +0100

    update werehouses controller

--- KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
+++ KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f619f0367be996322d6270c06d7febfe8a4eb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2606868746e7c8b160f64851e49c2311376ddd0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
