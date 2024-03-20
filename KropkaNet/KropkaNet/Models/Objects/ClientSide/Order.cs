342e92d9acdd2cda8af26895d4658b2abd6542e9
grochochotowski <g.m.rochowski@gmail.com>
2024-03-20 15:33:29 +0100

    naming v2

--- KropkaNet/KropkaNet/Models/Objects/ClientSide/Order.cs
+++ KropkaNet/KropkaNet/Models/Objects/ClientSide/Order.cs
@@ -19,7 +19,7 @@ namespace KropkaNet.Models.Objects.ClientSide
 
 
         [Required(ErrorMessage = "Stocktaking is required")]
-        public int StocktakingIT { get; set; }
+        public int StocktakingId { get; set; }
         public virtual Stocktaking Stocktaking { get; set; }
 
 
