47ffe3fce5a4bf8636c2254902c3c2fa4793a7df
karichil <karina.chilkiewicz@gmail.com>
2024-03-24 21:07:21 +0100

    update address dto

--- KropkaNet/KropkaNet/Models/Dtos/ClientSide/Address/UpdateAddressDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/ClientSide/Address/UpdateAddressDto.cs
@@ -2,5 +2,17 @@
 {
     public class UpDateAddressDto
     {
+        public string? Country { get; set; }
+
+        public string? City { get; set; }
+
+        public string? ZipCode { get; set; }
+
+        public string? Street { get; set; }
+
+        public string? Building { get; set; }
+        public string? Premises { get; set; }
     }
 }
+
+
