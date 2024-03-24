c30f870a7a9d18088658e8c951991594b777b34a
karichil <karina.chilkiewicz@gmail.com>
2024-03-24 21:10:15 +0100

    update company dto

--- KropkaNet/KropkaNet/Models/Dtos/ClientSide/Company/CompanyDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/ClientSide/Company/CompanyDto.cs
@@ -1,6 +1,25 @@
-﻿namespace KropkaNet.Models.Dtos.ClientSide.Company
+﻿using KropkaNet.Models.Dtos.ClientSide.Address;
+using KropkaNet.Models.Dtos.ClientSide.Department;
+using KropkaNet.Models.Dtos.ClientSide.User;
+
+namespace KropkaNet.Models.Dtos.ClientSide.Company
 {
     public class CompanyDto
     {
+        public int Id { get; set; }
+        
+        public int NIP { get; set; }
+       
+        public int KRS { get; set; }
+        
+        public string CompanyName { get; set; }
+        public string? Note { get; set; }
+
+
+        public int AddressId { get; set; }
+        public virtual AddressDto Address { get; set; }
+
+        public virtual ICollection<DepartmentDto>? Departments { get; set; }
+        public virtual ICollection<UserDto> Users { get; set; }
     }
 }
--- KropkaNet/KropkaNet/Models/Dtos/ClientSide/Company/CreateCompanyDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/ClientSide/Company/CreateCompanyDto.cs
@@ -1,6 +1,18 @@
-﻿namespace KropkaNet.Models.Dtos.ClientSide.Company
+﻿using KropkaNet.Models.Dtos.ClientSide.Address;
+using KropkaNet.Models.Dtos.ClientSide.Department;
+using KropkaNet.Models.Dtos.ClientSide.User;
+
+namespace KropkaNet.Models.Dtos.ClientSide.Company
 {
     public class CreateCompanyDto
     {
+        public int NIP { get; set; }
+
+        public int KRS { get; set; }
+
+        public string CompanyName { get; set; }
+        public string? Note { get; set; }
+
+        public int AddressId { get; set; }
     }
 }
