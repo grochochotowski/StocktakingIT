af06ad65a7a2845752fe33300d5ffa44c2f82c15
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 20:45:52 +0100

    configure automapper

---
+++ KropkaNet/KropkaNet/StocktakingMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace KropkaNet
+{
+    public class StocktakingMappingProfile : Profile
+    {
+        public StocktakingMappingProfile()
+        {
+
+        }
+    }
+}
