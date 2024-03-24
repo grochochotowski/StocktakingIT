d036074a3b339f3734dc6ef8a9a241404843c629
karichil <karina.chilkiewicz@gmail.com>
2024-03-24 21:18:55 +0100

    update user dto

--- KropkaNet/KropkaNet/Models/Dtos/ClientSide/User/UpdateUserDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/ClientSide/User/UpdateUserDto.cs
@@ -2,5 +2,11 @@
 {
     public class UpdateUserDto
     {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? PersonalNumber { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Note { get; set; }
     }
 }
--- KropkaNet/KropkaNet/Models/Dtos/ClientSide/User/UserDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/ClientSide/User/UserDto.cs
@@ -1,6 +1,19 @@
-﻿namespace KropkaNet.Models.Dtos.ClientSide.User
+﻿using KropkaNet.Models.Dtos.ClientSide.Company;
+using KropkaNet.Models.Dtos.ClientSide.Order;
+
+namespace KropkaNet.Models.Dtos.ClientSide.User
 {
     public class UserDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PersonalNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Note { get; set; }
+
+        public virtual ICollection<OrderDto>? Orderds { get; set; }
+        public virtual ICollection<CompanyDto>? Companies { get; set; }
     }
 }
