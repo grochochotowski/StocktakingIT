f272004b67351e57d378b7b1ca999ca20c248876
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 20:50:05 +0100

    create folders for dto

--- KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
+++ KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8921b4e0b4f60c99ce109f5ee8b0d0b66b64a4d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af06ad65a7a2845752fe33300d5ffa44c2f82c15")]
 [assembly: System.Reflection.AssemblyProductAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
