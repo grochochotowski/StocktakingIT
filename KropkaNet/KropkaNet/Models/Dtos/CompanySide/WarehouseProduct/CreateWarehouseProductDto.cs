a6b627f7900f275cf2a2f384a9747a2c728524de
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 20:56:17 +0100

    create WarehouseProductDto files

---
+++ KropkaNet/KropkaNet/Models/Dtos/CompanySide/WarehouseProduct/CreateWarehouseProductDto.cs
@@ -0,0 +1,6 @@
+﻿namespace KropkaNet.Models.Dtos.CompanySide.WarehouseProduct
+{
+    public class CreateWarehouseProductDto
+    {
+    }
+}
--- KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
+++ KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a868b8696c47356a4cd78e62f5a963d598d88b3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed2d42d06369323df7a3a6584317ffe6fb41a18")]
 [assembly: System.Reflection.AssemblyProductAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
