1f381cabb35fbe87dd5f8927472cabf64a4e9002
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 21:22:29 +0100

    create mapping profiles for company side objects

--- KropkaNet/KropkaNet/StocktakingMappingProfile.cs
+++ KropkaNet/KropkaNet/StocktakingMappingProfile.cs
@@ -1,4 +1,11 @@
 ﻿using AutoMapper;
+using KropkaNet.Models.Dtos.CompanySide.Employee;
+using KropkaNet.Models.Dtos.CompanySide.Position;
+using KropkaNet.Models.Dtos.CompanySide.Product;
+using KropkaNet.Models.Dtos.CompanySide.Stocktaking;
+using KropkaNet.Models.Dtos.CompanySide.Warehouse;
+using KropkaNet.Models.Dtos.CompanySide.WarehouseProduct;
+using KropkaNet.Models.Objects.CompanySide;
 
 namespace KropkaNet
 {
@@ -6,7 +13,32 @@ namespace KropkaNet
     {
         public StocktakingMappingProfile()
         {
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<CreateEmployeeDto, Employee>();
+            CreateMap<UpdateEmployeeDto, Employee>();
 
+
+            CreateMap<Position, PositionDto>();
+
+
+            CreateMap<Product, ProductDto>();
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+
+
+            CreateMap<Stocktaking, StocktakingDto>();
+            CreateMap<CreateStocktakingDto, Stocktaking>();
+            CreateMap<UpdateStocktakingDto, Stocktaking>();
+
+
+            CreateMap<Warehouse, WarehouseDto>();
+            CreateMap<CreateWarehouseDto, Warehouse>();
+            CreateMap<UpdateWarehouseDto, Warehouse>();
+
+
+            CreateMap<WarehouseProduct, WarehouseProductDto>();
+            CreateMap<CreateWarehouseProductDto, WarehouseProduct>();
+            CreateMap<UpdateWarehouseProductDto, WarehouseProduct>();
         }
     }
 }
