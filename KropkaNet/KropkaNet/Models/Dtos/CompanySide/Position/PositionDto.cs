109e85de0adeea48a5250ba4f07dcee22e886ce7
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 21:03:35 +0100

    creaste position dto

---
+++ KropkaNet/KropkaNet/Models/Dtos/CompanySide/Position/PositionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace KropkaNet.Models.Dtos.CompanySide.Position
+{
+    public class PositionDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
