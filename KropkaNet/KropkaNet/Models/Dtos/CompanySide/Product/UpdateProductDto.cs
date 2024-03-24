150b5c719429bbf031346298f893fd6be35dcc45
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 20:52:57 +0100

    create product dto files

---
+++ KropkaNet/KropkaNet/Models/Dtos/CompanySide/Product/UpdateProductDto.cs
@@ -0,0 +1,6 @@
+﻿namespace KropkaNet.Models.Dtos.CompanySide.Product
+{
+    public class UpdateProductDto
+    {
+    }
+}
--- KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
+++ KropkaNet/KropkaNet/obj/Debug/net8.0/KropkaNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f272004b67351e57d378b7b1ca999ca20c248876")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee3b4a83abee5cca6c03bd9a0f36d668e064f2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KropkaNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
