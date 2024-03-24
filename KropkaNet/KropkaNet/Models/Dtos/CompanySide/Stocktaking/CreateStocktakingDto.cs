1aed555fe34aa0be06eb466025c93e7409e14a4a
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 21:12:11 +0100

    create stocktaking dtos

--- KropkaNet/KropkaNet/Models/Dtos/CompanySide/Stocktaking/CreateStocktakingDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/CompanySide/Stocktaking/CreateStocktakingDto.cs
@@ -1,6 +1,12 @@
-﻿namespace KropkaNet.Models.Dtos.CompanySide.Stocktaking
+﻿using KropkaNet.Models.Dtos.CompanySide.Warehouse;
+
+namespace KropkaNet.Models.Dtos.CompanySide.Stocktaking
 {
     public class CreateStocktakingDto
     {
+        public int ExpectedTimeHours { get; set; }
+        public string? Note { get; set; }
+        public int WarehouseId { get; set; }
+        public int OrderId { get; set; }
     }
 }
--- KropkaNet/KropkaNet/Models/Dtos/CompanySide/Stocktaking/UpdateStocktakingDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/CompanySide/Stocktaking/UpdateStocktakingDto.cs
@@ -2,5 +2,9 @@
 {
     public class UpdateStocktakingDto
     {
+        public int? ExpectedTimeHours { get; set; }
+        public string? Note { get; set; }
+        public int? WarehouseId { get; set; }
+        public int? OrderId { get; set; }
     }
 }
