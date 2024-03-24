18e26eba02f0680e65065d9bf60f5814db8e2922
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 21:04:57 +0100

    create employeedtos

--- KropkaNet/KropkaNet/Models/Dtos/CompanySide/Employee/CreateEmployeeDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/CompanySide/Employee/CreateEmployeeDto.cs
@@ -1,6 +1,16 @@
-﻿namespace KropkaNet.Models.Dtos.CompanySide.Employee
+﻿using KropkaNet.Models.Dtos.CompanySide.Position;
+using KropkaNet.Models.Dtos.CompanySide.Stocktaking;
+
+namespace KropkaNet.Models.Dtos.CompanySide.Employee
 {
     public class CreateEmployeeDto
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PersonalNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Note { get; set; }
+        public int PositionId { get; set; }
     }
 }
--- KropkaNet/KropkaNet/Models/Dtos/CompanySide/Employee/UpdateEmployeeDto.cs
+++ KropkaNet/KropkaNet/Models/Dtos/CompanySide/Employee/UpdateEmployeeDto.cs
@@ -2,5 +2,12 @@
 {
     public class UpdateEmployeeDto
     {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? PersonalNumber { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Note { get; set; }
+        public int? PositionId { get; set; }
     }
 }
