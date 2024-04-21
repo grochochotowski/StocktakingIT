add0646a221c25c429c71d7a395f022e1c15d41e
grochochotowski <g.m.rochowski@gmail.com>
2024-04-21 21:41:24 +0200

    cpy files from mvc app

---
+++ KropkaNetApi/KropkaNetApi/PositionSeeder.cs
@@ -0,0 +1,40 @@
+﻿using KropkaNet.Models.Objects.CompanySide;
+using KropkaNet.Models.system;
+
+namespace KropkaNet
+{
+    public class PositionSeeder
+    {
+        private readonly StocktakingContext _context;
+
+        public PositionSeeder(StocktakingContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Database.CanConnect())
+            {
+                if (!_context.Positions.Any())
+                {
+                    var positions = GetPositions();
+                    _context.Positions.AddRange(positions);
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+        public IEnumerable<Position> GetPositions()
+        {
+            var postions = new List<Position>()
+            {
+                new Position { Name = "Employee"},
+                new Position { Name = "Moderator"},
+                new Position { Name = "Admin"}
+            };
+
+            return postions;
+        }
+    }
+}
---
+++ KropkaNetApi/KropkaNetApi/StocktakingMappingProfile.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using KropkaNet.Models.Dtos.ClientSide.Address;
+using KropkaNet.Models.Dtos.ClientSide.Company;
+using KropkaNet.Models.Dtos.ClientSide.Department;
+using KropkaNet.Models.Dtos.ClientSide.Order;
+using KropkaNet.Models.Dtos.ClientSide.User;
+using KropkaNet.Models.Objects.ClientSide;
+using KropkaNet.Models.Dtos.CompanySide.Employee;
+using KropkaNet.Models.Dtos.CompanySide.Position;
+using KropkaNet.Models.Dtos.CompanySide.Product;
+using KropkaNet.Models.Dtos.CompanySide.Stocktaking;
+using KropkaNet.Models.Dtos.CompanySide.Warehouse;
+using KropkaNet.Models.Dtos.CompanySide.WarehouseProduct;
+using KropkaNet.Models.Objects.CompanySide;
+
+namespace KropkaNet
+{
+    public class StocktakingMappingProfile : Profile
+    {
+        public StocktakingMappingProfile()
+        {
+            CreateMap<Address, AddressDto>();
+            CreateMap<CreateAddressDto, Address>();
+            CreateMap<UpdateAddressDto, Address>();
+
+            CreateMap<Company, CompanyDto>();
+            CreateMap<CreateCompanyDto, Company>();
+            CreateMap<UpdateCompanyDto, Company>();
+
+            CreateMap<Department, DepartmentDto>();
+            CreateMap<CreateDepartmentDto, Department>();
+            CreateMap<UpdateDepartmentDto, Department>();
+
+            CreateMap<Order, OrderDto>();
+            CreateMap<CreateOrderDto, Order>();
+            CreateMap<UpdateOrderDto, Order>();
+
+            CreateMap<User, UserDto>();
+            CreateMap<CreateUserDto, User>();
+            CreateMap<UpdateUserDto, User>();
+
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<CreateEmployeeDto, Employee>();
+            CreateMap<UpdateEmployeeDto, Employee>();
+
+
+            CreateMap<Position, PositionDto>();
+
+
+            CreateMap<Product, ProductDto>();
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+
+
+            CreateMap<Stocktaking, StocktakingDto>();
+            CreateMap<CreateStocktakingDto, Stocktaking>();
+            CreateMap<UpdateStocktakingDto, Stocktaking>();
+
+
+            CreateMap<Warehouse, WarehouseDto>();
+            CreateMap<CreateWarehouseDto, Warehouse>();
+            CreateMap<UpdateWarehouseDto, Warehouse>();
+
+
+            CreateMap<WarehouseProduct, WarehouseProductDto>();
+            CreateMap<CreateWarehouseProductDto, WarehouseProduct>();
+            CreateMap<UpdateWarehouseProductDto, WarehouseProduct>();
+
+        }
+    }
+}
