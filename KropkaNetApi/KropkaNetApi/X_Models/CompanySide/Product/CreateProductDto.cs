616d513d26fd735c05e700be79de27e319728dd8
grochochotowski <g.m.rochowski@gmail.com>
2024-04-22 15:14:36 +0200

    create model for creating new product

---
+++ KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Product/CreateProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace KropkaNetApi.X_Models.CompanySide.Product
+{
+    public class CreateProductDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string? Note { get; set; }
+    }
+}
