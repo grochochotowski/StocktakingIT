6759b3df3bb9ed94ed32970d16e50094ab14f696
grochochotowski <g.m.rochowski@gmail.com>
2024-04-22 15:11:56 +0200

    create get list of products api method

---
+++ KropkaNetApi/KropkaNetApi/Y_Controllers/CompanySide/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using KropkaNetApi.X_Models.CompanySide.Product;
+using KropkaNetApi.Y_Services.CompanySide;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KropkaNetApi.Y_Controllers.CompanySide
+{
+    [Route("api/kropkaNet/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+
+        // GET /api/kropkaNet/product/list
+        [HttpGet("list")]
+        public ActionResult<IEnumerable<ProductListDto>> GetList([FromQuery] string? filter)
+        {
+            var productList = _productService.GetList(filter);
+            return Ok(productList);
+        }
+    }
+}
---
+++ KropkaNetApi/KropkaNetApi/Y_Services/CompanySide/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using KropkaNetApi.X_Entities;
+using KropkaNetApi.X_Models.CompanySide.Product;
+
+namespace KropkaNetApi.Y_Services.CompanySide
+{
+    public interface IProductService
+    {
+        IEnumerable<ProductListDto> GetList(string filter);
+    }
+
+    public class ProductService : IProductService
+    {
+        private StocktakingContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductService(StocktakingContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        // Get product list
+        public IEnumerable<ProductListDto> GetList(string filter)
+        {
+            var productList = _context.Products
+                .Where(
+                    p => filter == null || (
+                    p.Name.ToLower().Contains(filter) ||
+                    p.Category.ToLower().Contains(filter) ||
+                    p.Note.ToLower().Contains(filter) ||
+                    p.Id.ToString().Contains(filter)
+                ))
+                .OrderBy(p => p.Name)
+                .Select(p => new ProductListDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Category = p.Category
+                })
+                .ToList();
+
+            return productList;
+        }
+    }
+}
