5d922f5ce66344cabb7a6ef46115643dda393ff7
grochochotowski <g.m.rochowski@gmail.com>
2024-04-22 15:11:39 +0200

    create model for getting list of products

---
+++ KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Product/ProductListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KropkaNetApi.X_Models.CompanySide.Product
+{
+    public class ProductListDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+    }
+}
