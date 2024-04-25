4df6e39f03da8af5c5689986ac049886d31101e4
grochochotowski <g.m.rochowski@gmail.com>
2024-04-25 22:01:17 +0200

    make base of routing

--- KropkaNetFrontEnd/src/main.jsx
+++ KropkaNetFrontEnd/src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+import WelcomePage  from './pages/BeginPages/WelcomePage'
+import LoginPage  from './pages/BeginPages/LoginPage'
+import RegisterPage from './pages/BeginPages/RegisterPage'
+
+import NotFoundPage from './pages/ErrorPages/NotFoundPage'
+
+
+import './styles/index.css'
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <WelcomePage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/login',
+        element: <LoginPage  />
+    },
+    {
+        path: '/register',
+        element: <RegisterPage  />
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router}/>
     </React.StrictMode>,
 )
