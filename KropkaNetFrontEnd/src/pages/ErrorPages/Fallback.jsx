1ed37f860461683dab72c0454cbe346f69aa71de
grochochotowski <g.m.rochowski@gmail.com>
2024-04-26 22:52:16 +0200

    refactor

--- KropkaNetFrontEnd/src/pages/ErrorPages/Fallback.jsx
+++ KropkaNetFrontEnd/src/pages/ErrorPages/Fallback.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import '../../styles/error.css'
 
 function Fallback() {
-  return (
-    <div>Loading ...</div>
-  )
+    return (
+        <div className="error-container">
+            <h1>Loading ...</h1>
+        </div>
+    )
 }
 
 export default Fallback
