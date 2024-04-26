5e9ac7f789596140f5ad7b8056fd2b837f6bc62d
grochochotowski <g.m.rochowski@gmail.com>
2024-04-26 19:34:02 +0200

    create layout for navbar

--- KropkaNetFrontEnd/src/components/NavBar.jsx
+++ KropkaNetFrontEnd/src/components/NavBar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
+
+import '../styles/navbar.css'
 
 function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <nav className='closed'>
+            <div className='options'>
+                <Link to='/'>
+                    <h2><i className="fa-solid fa-house"></i></h2>
+                    <h4>Home</h4>
+                </Link>
+                <Link to='/orders'>
+                    <h2><i className="fa-solid fa-basket-shopping"></i></h2>
+                    <h4>Orders</h4>
+                </Link>
+                <Link to='/account'>
+                    <h2><i className="fa-solid fa-user"></i></h2>
+                    <h4>Account</h4>
+                </Link>
+                <Link to='/settings'>
+                    <h2><i className="fa-solid fa-gear"></i></h2>
+                    <h4>Setting</h4>
+                </Link>
+            </div>
+        </nav>
+    )
 }
 
 export default NavBar
