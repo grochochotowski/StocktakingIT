914e057be8e56a2635ae96297182cd9ab13788f1
grochochotowski <g.m.rochowski@gmail.com>
2024-04-27 23:44:33 +0200

    create base login form

--- KropkaNetFrontEnd/src/pages/BeginPages/LoginPage.jsx
+++ KropkaNetFrontEnd/src/pages/BeginPages/LoginPage.jsx
@@ -1,7 +1,34 @@
+import '../../styles/index.css'
+import '../../styles/form.css'
+
 function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+
+    const login = () => {
+        alert("Log in")
+    }
+    return (
+        <div className="main-container">
+            <form>
+                <div className="layer">
+                    <div className="input-container">
+                        <label htmlFor="login">Login:</label>
+                        <input
+                            type="text"
+                            id="login"
+                        />
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="text"
+                            id="password"
+                        />
+                    </div>
+                </div>
+                <button onClick={login}>Log in</button>
+            </form>
+        </div>
+    )
 }
 
 export default LoginPage
