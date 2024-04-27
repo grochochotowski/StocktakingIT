75c20b238b316dd521d925fb5378dcf18078b4d3
grochochotowski <g.m.rochowski@gmail.com>
2024-04-27 23:23:55 +0200

    create base layout for welcome page

--- KropkaNetFrontEnd/src/pages/BeginPages/WelcomePage.jsx
+++ KropkaNetFrontEnd/src/pages/BeginPages/WelcomePage.jsx
@@ -1,6 +1,23 @@
+import {Link} from 'react-router-dom'
+
+import logo from '../../assets/logo.png'
+
+import '../../styles/index.css'
+
 function WelcomePage() {
     return (
-        <div>WelcomePage</div>
+        <div className="main-container">
+            <main className="welcome">
+                <div className="logo">
+                    <img src={logo} alt="kropka-net-logo" />
+                </div>
+                <div className="controlls">
+                    <Link to="/login" className="button">Log in</Link>
+                    <Link to="/register" className="button">Register</Link>
+                </div>
+                <div className="bg"></div>
+            </main>
+        </div>
     )
 }
 
