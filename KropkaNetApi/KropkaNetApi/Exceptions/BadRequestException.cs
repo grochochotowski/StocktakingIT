390ea0470cadeb214a3794be79901ec13b408e66
grochochotowski <g.m.rochowski@gmail.com>
2024-04-29 15:10:50 +0200

    create bad request exception

---
+++ KropkaNetApi/KropkaNetApi/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿namespace KropkaNetApi.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ KropkaNetApi/KropkaNetApi/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using KropkaNetApi.Exceptions;
+
+namespace KropkaNetApi.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}
