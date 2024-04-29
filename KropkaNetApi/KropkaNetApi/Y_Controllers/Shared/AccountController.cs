c703fe309249ed850a24cdf2b056ae500f5dbd92
grochochotowski <g.m.rochowski@gmail.com>
2024-04-29 15:11:06 +0200

    create api method for login

---
+++ KropkaNetApi/KropkaNetApi/Y_Controllers/Shared/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using KropkaNetApi.X_Models.Shared.Account;
+using KropkaNetApi.Y_Services.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KropkaNetApi.Y_Controllers.Shared
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] LoginDto dto)
+        {
+            string token = _accountService.GenerateToken(dto, HttpContext);
+
+            return Ok(new { token = token });
+        }
+
+    }
+}
+    
