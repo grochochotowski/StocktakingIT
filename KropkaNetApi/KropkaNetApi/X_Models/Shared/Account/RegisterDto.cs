071c3d0b1a69c714f708eeb9b35e4aec354a7aca
grochochotowski <g.m.rochowski@gmail.com>
2024-04-29 23:39:53 +0200

    create registe model

---
+++ KropkaNetApi/KropkaNetApi/X_Models/Shared/Account/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KropkaNetApi.X_Models.Shared.Account
+{
+    public class RegisterDto
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
