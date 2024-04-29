6f4d01fe70d6978855dce4a376a004a2c96bebe8
grochochotowski <g.m.rochowski@gmail.com>
2024-04-29 15:11:34 +0200

    add authorization

---
+++ KropkaNetApi/KropkaNetApi/X_Entities/AuthenticationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace KropkaNetApi.X_Entities
+{
+    public class AuthenticationSettings
+    {
+        public string JwtKey { get; set; }
+        public int JwtExpireDays { get; set; }
+        public string JwtIssuer { get; set; }
+    }
+}
