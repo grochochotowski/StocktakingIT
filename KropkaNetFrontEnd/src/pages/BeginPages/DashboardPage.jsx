e18fc4c0c259856baa641f8ad44e4b4e259c51dd
grochochotowski <g.m.rochowski@gmail.com>
2024-04-29 12:16:27 +0200

    create base api call to get example data

--- KropkaNetFrontEnd/src/pages/BeginPages/DashboardPage.jsx
+++ KropkaNetFrontEnd/src/pages/BeginPages/DashboardPage.jsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react';
+import instance from "../../api/axios"
+
 import NavBar from '../../components/NavBar'
 
 function DashboardPage() {
-  return (
-    <>
-        <NavBar />
-        <div>DashboardPage</div>
-    </>
-  )
+
+    const [restult, setResult] = useState([])
+
+    async function fetchData() {
+        let apiCall = `kropkaNet/product/list`
+        try {
+            const response = await instance().get(apiCall);
+            setResult(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <NavBar />
+            <div>DashboardPage</div>
+        </>
+    )
 }
 
 export default DashboardPage
