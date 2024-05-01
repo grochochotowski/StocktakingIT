210279fc4328df656f321ace6379a43e2ab48c47
grochochotowski <g.m.rochowski@gmail.com>
2024-05-01 22:10:16 +0200

    remove not needed parts of axios config

--- KropkaNetFrontEnd/src/api/axios.jsx
+++ KropkaNetFrontEnd/src/api/axios.jsx
@@ -2,23 +2,9 @@ import axios from 'axios';
 
 const baseURL = 'https://localhost:7238/api'
 
-// Retrieve the JWT token from the cookie
-const jwtCookie = document.cookie
-    .split(';')
-    .map(cookie => cookie.trim())
-    .find(cookie => cookie.startsWith('jwtToken='))
-
-let jwtToken = '';
-if (jwtCookie) {
-    jwtToken = jwtCookie.split('=')[1]
-}
-
 export default function instance() {
     return axios.create({
         baseURL,
-        withCredentials: true,
-        headers: {
-            'Authorization': `Bearer ${jwtToken}`
-          }
+        withCredentials: true
     });
 }
