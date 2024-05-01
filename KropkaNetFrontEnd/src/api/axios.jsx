227a3e3b5dd4a0bcc5c79010f43c65e68346c9e6
grochochotowski <g.m.rochowski@gmail.com>
2024-05-01 22:58:28 +0200

    remove not needed axios hooks

--- KropkaNetFrontEnd/src/api/axios.jsx
+++ KropkaNetFrontEnd/src/api/axios.jsx
@@ -3,10 +3,6 @@ import axios from 'axios';
 const BASEURL = 'https://localhost:7238/api'
 
 export default axios.create({
-    baseURL : BASEURL
-})
-
-export const axiosPrivate = axios.create({
     baseURL : BASEURL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
