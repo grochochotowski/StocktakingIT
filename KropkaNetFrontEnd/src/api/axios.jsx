4eeedf908cc78d08b8bfddaea2bed6fd0c4959c7
grochochotowski <g.m.rochowski@gmail.com>
2024-05-01 23:31:47 +0200

    update axios headers

--- KropkaNetFrontEnd/src/api/axios.jsx
+++ KropkaNetFrontEnd/src/api/axios.jsx
@@ -4,6 +4,8 @@ const BASEURL = 'https://localhost:7238/api'
 
 export default axios.create({
     baseURL : BASEURL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    withCredentials: true,
 })
