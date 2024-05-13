f35f267de419316f2bb01ba41a011d283ece294a
grochochotowski <g.m.rochowski@gmail.com>
2024-05-13 15:11:22 +0200

    create test data for orders page

--- KropkaNetFrontEnd/src/pages/OrderPages/OrderPage.jsx
+++ KropkaNetFrontEnd/src/pages/OrderPages/OrderPage.jsx
@@ -7,6 +7,38 @@ import '../../styles/order.css'
 
 function OrderPage() {
 
+    const [selected, setSelected] = useState(1);
+
+    const [orders, setOrders] = useState([
+        {
+        "id" : 1,
+        "dateOfOrderExecution" : "01/01/0001",
+        "departmentName" : "Department 1"
+        },
+        {
+        "id" : 2,
+        "dateOfOrderExecution" : "02/02/0002",
+        "departmentName" : "Department 2"
+        },
+        {
+        "id" : 3,
+        "dateOfOrderExecution" : "03/03/0003",
+        "departmentName" : "Department 3"
+        },
+        {
+        "id" : 4,
+        "dateOfOrderExecution" : "04/04/0004",
+        "departmentName" : "Department 4"
+        },
+        {
+        "id" : 5,
+        "dateOfOrderExecution" : "05/05/0005",
+        "departmentName" : "Department 5"
+        },
+    ])
+
+    
+
 
     return (
         <>
