effe6a4e26025c1607177a443e3ee41787088fc5
karichil <karina.chilkiewicz@gmail.com>
2024-05-13 14:41:01 +0200

    create CreateOrderDto and OrderListDto files

---
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Order/OrderListDto.cs
@@ -0,0 +1,10 @@
+﻿namespace KropkaNetApi.X_Models.ClientSide.Order
+{
+    public class OrderListDto
+    {
+        public int Id { get; set; }
+        public DateTime DateOfOrderExecution { get; set; }
+        public int DepartmentId { get; set; }
+        public int StocktakingId { get; set; }
+    }
+}
