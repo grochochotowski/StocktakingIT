aba0b9f341c95f53e21e38dbb6e01765cf8a0b8b
karichil <karina.chilkiewicz@gmail.com>
2024-05-13 14:35:58 +0200

    create CreateAddressDto and AddressListDto files

---
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Address/AddressListDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KropkaNetApi.X_Models.ClientSide.Address
+{
+    public class AddressListDto
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+        public string? Premises { get; set; }
+    }
+}
