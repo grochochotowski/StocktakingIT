b26c1589db6bd02dc4120d23657b7d2b82b2283a
grochochotowski <g.m.rochowski@gmail.com>
2024-05-13 15:10:49 +0200

    create base layout for orders page

--- KropkaNetFrontEnd/src/pages/OrderPages/OrderPage.jsx
+++ KropkaNetFrontEnd/src/pages/OrderPages/OrderPage.jsx
@@ -7,11 +7,21 @@ import '../../styles/order.css'
 
 function OrderPage() {
 
-    
-
 
     return (
         <>
+            <NavBar />
+            <div className="order-container">
+                <div className="list">
+                    {generateHeader}
+                    {generateBody}
+                </div>
+                <div className="options">
+                    <Link to="/orders/list">List</Link>
+                    <Link to={`/orders/details/${selected}`} className={selected ? "" : "disable"}>Details</Link>
+                    <Link to={`/orders/edit/${selected}`} className={selected ? "" : "disable"}>Edit</Link>
+                </div>
+            </div>
         </>
     )
 }
