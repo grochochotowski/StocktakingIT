b0685eb2394c59ae28ee0e4c83af1482fa23a80c
karichil <karina.chilkiewicz@gmail.com>
2024-05-13 14:42:38 +0200

    create CreateUserDto and UserListDto files

---
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/User/CreateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace KropkaNetApi.X_Models.ClientSide.User
+{
+    public class CreateUserDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PersonalNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Note { get; set; }
+    }
+}
---
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/User/UserListDto.cs
@@ -0,0 +1,12 @@
+﻿namespace KropkaNetApi.X_Models.ClientSide.User
+{
+    public class UserListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PersonalNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
