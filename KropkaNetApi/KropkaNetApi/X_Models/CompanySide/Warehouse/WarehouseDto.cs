6085e1a6937b35d89ed7ba585b87d33d7ad38cc0
graccz103 <graccz103@gmail.com>
2024-05-13 15:35:33 +0200

    add WarehouseListDto

--- KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Warehouse/WarehouseDto.cs
+++ KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Warehouse/WarehouseDto.cs
@@ -1,18 +1,9 @@
-﻿using KropkaNetApi.X_Models.CompanySide.Stocktaking;
-using KropkaNetApi.X_Models.CompanySide.WarehouseProduct;
-
-namespace KropkaNetApi.X_Models.CompanySide.Warehouse
+﻿namespace KropkaNetApi.X_Models.CompanySide.Warehouse
 {
-    public class WarehouseDto
+    public class WarehouseListDto
     {
         public int Id { get; set; }
         public string? Note { get; set; }
-
-
-        public int StocktakingId { get; set; }
-        public virtual StocktakingDto Stocktaking { get; set; }
-
-
-        public virtual ICollection<WarehouseProductDto> WarehouseProducts { get; set; }
+        public int StocktakingId { get; set; } 
     }
 }
