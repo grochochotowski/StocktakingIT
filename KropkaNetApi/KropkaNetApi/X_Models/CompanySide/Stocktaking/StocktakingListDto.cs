b52680cbd943f7ba7a062c0692cf164ee780cc71
graccz103 <graccz103@gmail.com>
2024-05-13 15:21:47 +0200

    add StocktakingListDto

---
+++ KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Stocktaking/StocktakingListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KropkaNetApi.X_Models.CompanySide.Stocktaking
+{
+    public class StocktakingListDto
+    {
+        public int Id { get; set; }
+        public int ExpectedTimeHours { get; set; }
+        public string? Note { get; set; }
+    }
+}
