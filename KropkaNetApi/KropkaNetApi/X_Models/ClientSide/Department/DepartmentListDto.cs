c9d8617c8181168dc796a4d035542ddc392253f4
karichil <karina.chilkiewicz@gmail.com>
2024-05-13 14:38:52 +0200

    create CreateDepartmentDto and DepartmentListDto files

---
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Department/DepartmentListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KropkaNetApi.X_Models.ClientSide.Department
+{
+    public class DepartmentListDto
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
