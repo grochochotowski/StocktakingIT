313fa5cdad9a2700478b844421cba8316eefb12e
karichil <karina.chilkiewicz@gmail.com>
2024-05-13 14:33:34 +0200

    create ComapnyListDto file

---
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Company/ComapnyListDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KropkaNetApi.X_Models.ClientSide.Company
+{
+    public class ComapnyListDto
+    {
+        public int Id { get; set; }
+        public int NIP { get; set; }
+        public int KRS { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
