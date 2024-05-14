986f3e6c032a40247c4eb55eeaffcc029996e02b
graccz103 <graccz103@gmail.com>
2024-05-14 10:52:52 +0200

    fixed CreateStocktakingDto

--- KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Stocktaking/CreateStocktakingDto.cs
+++ KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Stocktaking/CreateStocktakingDto.cs
@@ -4,7 +4,5 @@
     {
         public int ExpectedTimeHours { get; set; }
         public string? Note { get; set; }
-        public int WarehouseId { get; set; }
-        public int OrderId { get; set; }
     }
 }
