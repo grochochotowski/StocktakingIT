ca97bb87524ae2975f81422143828e95861bee32
grochochotowski <g.m.rochowski@gmail.com>
2024-05-22 23:52:56 +0200

    create resgister user

--- KropkaNetApi/KropkaNetApi/Y_Controllers/Shared/AccountController.cs
+++ KropkaNetApi/KropkaNetApi/Y_Controllers/Shared/AccountController.cs
@@ -19,6 +19,18 @@ namespace KropkaNetApi.Y_Controllers.Shared
 
 
 
+        [HttpPost("employee/register")]
+        public ActionResult RegisterEmployee([FromBody] RegisterEmployeeDto dto)
+        {
+            _accountService.RegisterEmployee(dto);
+            return Ok();
+        }
+        [HttpPost("user/register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+            _accountService.RegisterUser(dto);
+            return Ok();
+        }
         [HttpPost("login")]
         public ActionResult LogIn([FromBody] LoginDto dto)
         {
@@ -31,7 +43,6 @@ namespace KropkaNetApi.Y_Controllers.Shared
 
             return Unauthorized("Login or password is incorrect");
         }
-
         [HttpPost("refresh")]
         public ActionResult Refresh([FromBody] RefreshTokenModel model)
         {
