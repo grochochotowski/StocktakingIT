fe94a7b6c0a819b7730be3ef345568d951f031ab
grochochotowski <g.m.rochowski@gmail.com>
2024-05-22 23:27:08 +0200

    register employee

---
+++ KropkaNetApi/KropkaNetApi/Y_Controllers/CompanySide/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using KropkaNetApi.X_Models.CompanySide.Employee;
+using KropkaNetApi.X_Models.CompanySide.Product;
+using KropkaNetApi.Y_Services.CompanySide;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KropkaNetApi.Y_Controllers.CompanySide
+{
+    [Route("api/company/employee")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+
+        // POST: /api/company/employee/register
+        [HttpPost("register")]
+        public ActionResult Register([FromBody] RegisterEmployeeDto dto)
+        {
+            _employeeService.Register(dto);
+            return Ok();
+        }
+    }
+}
