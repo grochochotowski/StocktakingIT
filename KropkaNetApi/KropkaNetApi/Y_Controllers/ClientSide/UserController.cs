d6e630def5fe98962eb8f7837a5d67cda2827397
grochochotowski <g.m.rochowski@gmail.com>
2024-05-22 23:52:44 +0200

    create user service and controller

---
+++ KropkaNetApi/KropkaNetApi/Y_Controllers/ClientSide/UserController.cs
@@ -0,0 +1,21 @@
+﻿using KropkaNetApi.X_Models.CompanySide.Employee;
+using KropkaNetApi.X_Models.Shared.Account;
+using KropkaNetApi.Y_Services.ClientSide;
+using KropkaNetApi.Y_Services.CompanySide;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KropkaNetApi.Y_Controllers.ClientSide
+{
+    [Route("api/kropkaNet/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+    
+}
---
+++ KropkaNetApi/KropkaNetApi/Y_Services/ClientSide/UserService.cs
@@ -0,0 +1,27 @@
+﻿using KropkaNetApi.Exceptions;
+using KropkaNetApi.X_Entities.Objects.CompanySide;
+using KropkaNetApi.X_Entities;
+using KropkaNetApi.X_Models.CompanySide.Employee;
+using Microsoft.AspNetCore.Identity;
+using KropkaNetApi.X_Entities.Objects.Shared;
+using KropkaNetApi.X_Entities.Objects.ClientSide;
+using KropkaNetApi.X_Models.Shared.Account;
+
+namespace KropkaNetApi.Y_Services.ClientSide
+{
+    public interface IUserService
+    {
+        
+    }
+    public class UserService : IUserService
+    {
+        private readonly StocktakingContext _context;
+        private readonly IPasswordHasher<Account> _passwordHasher;
+
+        public UserService(StocktakingContext context, IPasswordHasher<Account> passwordHasher, AuthenticationSettings authenticationSettings)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+    }
+}
