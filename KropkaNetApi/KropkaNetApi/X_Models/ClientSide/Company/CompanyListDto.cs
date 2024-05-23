bb7a0f4421c9ff9bd998785e9920b90d65c37b44
grochochotowski <g.m.rochowski@gmail.com>
2024-05-23 22:07:48 +0200

    change type of nip and krs

--- KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Company/CompanyListDto.cs
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Company/CompanyListDto.cs
@@ -6,8 +6,6 @@ namespace KropkaNetApi.X_Models.ClientSide.Company
     public class CompanyListDto
     {
         public int Id { get; set; }
-        public int NIP { get; set; }
-        public int KRS { get; set; }
         public string CompanyName { get; set; }
     }
 }
--- KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Company/CreateCompanyDto.cs
+++ KropkaNetApi/KropkaNetApi/X_Models/ClientSide/Company/CreateCompanyDto.cs
@@ -4,10 +4,16 @@ namespace KropkaNetApi.X_Models.ClientSide.Company
 {
     public class CreateCompanyDto
     {
-        public int NIP { get; set; }
-        public int KRS { get; set; }
+        public string NIP { get; set; }
+        public string KRS { get; set; }
         public string CompanyName { get; set; }
         public string? Note { get; set; }
-        public int AddressId { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+        public string? Premises { get; set; }
     }
 }
