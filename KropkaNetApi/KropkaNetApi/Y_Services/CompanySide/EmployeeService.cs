3ce4873774619d892c9a58e20322fe3b31f1502b
karichil <karina.chilkiewicz@gmail.com>
2024-05-25 13:31:22 +0200

    cerate Create service in employee service file

--- KropkaNetApi/KropkaNetApi/Y_Services/CompanySide/EmployeeService.cs
+++ KropkaNetApi/KropkaNetApi/Y_Services/CompanySide/EmployeeService.cs
@@ -2,20 +2,25 @@
 using KropkaNetApi.X_Entities;
 using KropkaNetApi.X_Entities.Objects.CompanySide;
 using KropkaNetApi.X_Entities.Objects.Shared;
+using KropkaNetApi.X_Models.CompanySide.Employee;
 using KropkaNetApi.X_Models.CompanySide.Product;
 using KropkaNetApi.X_Models.Shared.Account;
 using Microsoft.AspNetCore.Identity;
+using AutoMapper;
 
 namespace KropkaNetApi.Y_Services.CompanySide
 {
     public interface IEmployeeService
     {
-        
+        public int Create(CreateEmployeeDto dto);
+
+
     }
     public class EmployeeService : IEmployeeService
     {
         private readonly StocktakingContext _context;
         private readonly IPasswordHasher<Account> _passwordHasher;
+        private readonly IMapper _mapper;
 
         public EmployeeService(StocktakingContext context, IPasswordHasher<Account> passwordHasher, AuthenticationSettings authenticationSettings)
         {
@@ -23,6 +28,26 @@ namespace KropkaNetApi.Y_Services.CompanySide
             _passwordHasher = passwordHasher;
         }
 
-        public int Create()
+        public int Create(CreateEmployeeDto dto)
+        {
+            var employee = _mapper.Map<Employee>(dto);
+
+            var position = new Position()
+            {
+                Name = dto.Name
+            };
+
+            _context.Positions.Add(position);
+            _context.SaveChanges();
+
+            employee.PositionId=position.Id;
+
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+
+            return employee.Id;
+        }
+
+        public ReturnResult<Employee>
     }
 }
