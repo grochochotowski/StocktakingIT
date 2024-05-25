a22f4b60e736190a489bb0045e4f34750f07eaae
karichil <karina.chilkiewicz@gmail.com>
2024-05-25 13:15:39 +0200

    add services to department service and edit department controller

--- KropkaNetApi/KropkaNetApi/Y_Controllers/ClientSide/DepartmentController.cs
+++ KropkaNetApi/KropkaNetApi/Y_Controllers/ClientSide/DepartmentController.cs
@@ -1,4 +1,6 @@
 ﻿using Azure;
+using KropkaNetApi.X_Entities.Enum;
+using KropkaNetApi.X_Models.ClientSide.Company;
 using KropkaNetApi.X_Models.ClientSide.Department;
 using KropkaNetApi.Y_Services.ClientSide;
 using Microsoft.AspNetCore.Authorization;
@@ -19,6 +21,7 @@ namespace KropkaNetApi.Y_Controllers.ClientSide
         }
 
         [HttpPost("create")]
+        [Authorize]
         public ActionResult Create([FromBody] CreateDepartmentDto dto)
         {
             var createdDepartmentId = _departmentService.Create(dto);
@@ -30,6 +33,51 @@ namespace KropkaNetApi.Y_Controllers.ClientSide
             }
 
             return result;
+        }
+        // GET api/kropkaNet/departemnt/all
+        [HttpGet("all")]
+        [Authorize(Roles = "Employee, Moderator, Admin")]
+        public ActionResult<IEnumerable<DepartmentDto>> GetList(
+            [FromQuery] int page,
+            [FromQuery] string? filters,
+            [FromQuery] string? sortBy,
+            [FromQuery] SortDirection sortDireciton
+            )
+        {
+            var departmentDtos = _departmentService.GetList(page, filters, sortBy, sortDireciton);
+            return Ok(departmentDtos);
+        }
+        // GET api/kropkaNet/departemnt/order/{id}
+        [HttpGet("user/{orderId}")]
+        [Authorize]
+        public ActionResult<IEnumerable<DepartmentDto>> GetListOrder(
+            [FromRoute] int orderId,
+            [FromQuery] int page,
+            [FromQuery] string? filters,
+            [FromQuery] string? sortBy,
+            [FromQuery] SortDirection sortDireciton
+            )
+        {
+            var departmentDtos = _departmentService.GetListOrder(orderId, page, filters, sortBy, sortDireciton);
+            return Ok(departmentDtos);
+        }
+        // PUT api/kropkaNet/department/update/5
+        [HttpPut("update/{id}")]
+        [Authorize]
+        public ActionResult Update([FromRoute] int id, [FromBody] CreateDepartmentDto dto)
+        {
+            var departmentId = _departmentService.Update(id, dto);
+
+            return Ok($"{departmentId}");
+        }
+        // DELETE api/kropkaNet/department/delete{id}
+        [HttpDelete("delete/{id}")]
+        [Authorize]
+        public ActionResult<IEnumerable<DepartmentDto>> Delete([FromRoute] int id)
+        {
+            var departmentDtos = _departmentService.Delete(id);
+
+            return NoContent();
         }
     }
 }
--- KropkaNetApi/KropkaNetApi/Y_Services/ClientSide/DepartmentService.cs
+++ KropkaNetApi/KropkaNetApi/Y_Services/ClientSide/DepartmentService.cs
@@ -5,14 +5,18 @@ using KropkaNetApi.X_Models.ClientSide.Department;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 using KropkaNetApi.Exceptions;
 using Microsoft.EntityFrameworkCore;
+using KropkaNetApi.X_Entities.Enum;
+using KropkaNetApi.X_Models.ClientSide.Company;
+using System.Linq.Expressions;
 
 namespace KropkaNetApi.Y_Services.ClientSide
 {
     public interface IDepartmentService
     {
         int Create(int? comapnyId, CreateDepartmentDto dto);
-        IEnumerable<DepartmentListDto> GetList(string filter);
-        void RemoveOrder(int comapnyId, int departmentId);
+        public ReturnResult<DepartmentListDto> GetList(int page, string filter, string sortBy, SortDirection sortDireciton);
+        ReturnResult<DepartmentListDto> GetListOrder(int orderId, int page, string filter, string sortBy, SortDirection sortDireciton);
+        public int Update(int id, CreateDepartmentDto dto);
         int Delete(int id);
     }
     public class DepartmentService : IDepartmentService
@@ -39,15 +43,33 @@ namespace KropkaNetApi.Y_Services.ClientSide
             return department.Id;
         }
 
-        // GET: get list of departments
-        public IEnumerable<DepartmentListDto> GetList(string filter)
+        // GET: get list of all departemnts
+        public ReturnResult<DepartmentListDto> GetList(int page, string filter, string sortBy, SortDirection sortDireciton)
         {
-            var departmentList = _context.Departments
-                .Where(
-                    p => filter == null || (
-                    p.DepartmentName.ToLower().Contains(filter) ||
-                    p.Id.ToString().Contains(filter)
-                ))
+            var baseQuery = _context.Departments
+                .Where(c => (string.IsNullOrEmpty(filter) || (
+                       c.DepartmentName.ToLower().Contains(filter.ToLower()) ||
+                       c.Id.ToString().Contains(filter))
+                       ));
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                var columnsSelector = new Dictionary<string, Expression<Func<Department, object>>>
+                {
+                    { "id", d => d.Id},
+                    { "DepartmentName", d => d.DepartmentName}
+                };
+
+                var selectedColumn = columnsSelector[sortBy];
+
+                baseQuery = sortDireciton == SortDirection.ASC
+                    ? baseQuery.OrderBy(selectedColumn)
+                    : baseQuery.OrderByDescending(selectedColumn);
+            }
+
+            var items = baseQuery
+                .Skip(10 * (page - 1))
+                .Take(10)
                 .OrderBy(p => p.DepartmentName)
                 .Select(p => new DepartmentListDto
                 {
@@ -56,24 +78,70 @@ namespace KropkaNetApi.Y_Services.ClientSide
                 })
                 .ToList();
 
-            return departmentList;
+            var totalCount = baseQuery.Count();
+
+            var result = new ReturnResult<DepartmentListDto>(items, totalCount);
+
+            return result;
         }
 
-        // PATCH: remove user
-        public void RemoveOrder(int comapnyId, int departmentId)
+        //GET : get list of orders
+
+        public ReturnResult<DepartmentListDto> GetListOrder(int orderId, int page, string filter, string sortBy, SortDirection sortDireciton)
+        {
+            var baseQuery = _context.Departments
+                .Include(c => c.Orders)
+                .Where(c => (string.IsNullOrEmpty(filter) || (
+                       c.DepartmentName.ToLower().Contains(filter.ToLower()) ||
+                       c.Id.ToString().Contains(filter))) &&
+                       c.Orders.Any(u => u.Id == orderId));
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                var columnsSelector = new Dictionary<string, Expression<Func<Department, object>>>
+                {
+                    { "id", c => c.Id},
+                    { "DepartmentName", c => c.DepartmentName}
+                };
+
+                var selectedColumn = columnsSelector[sortBy];
+
+                baseQuery = sortDireciton == SortDirection.ASC
+                    ? baseQuery.OrderBy(selectedColumn)
+                    : baseQuery.OrderByDescending(selectedColumn);
+            }
+
+            var items = baseQuery
+                .Skip(10 * (page - 1))
+                .Take(10)
+                .OrderBy(p => p.DepartmentName)
+                .Select(p => new DepartmentListDto
+                {
+                    Id = p.Id,
+                    DepartmentName = p.DepartmentName
+                })
+                .ToList();
+
+            var totalCount = baseQuery.Count();
+
+            var result = new ReturnResult<DepartmentListDto>(items, totalCount);
+
+            return result;
+        }
+
+        // PUT: update department
+        public int Update(int id, CreateDepartmentDto dto)
         {
             var department = _context.Departments
-                .Include(c => c.Companies)
-                .FirstOrDefault(c => c.Id == departmentId);
-            var company = _context.Companies
-                .FirstOrDefault(c => c.Id == comapnyId);
+                .FirstOrDefault(d => d.Id == id);
 
-            if (department == null) throw new NotFoundException("Company not found");
-            if (company == null) throw new NotFoundException("User not found");
-            if (!department.Companies.Any(u => u.Id == company.Id)) throw new BadRequestException("User is not in company");
+            if (department == null) throw new NotFoundException("Department not found");
 
-            department.Companies.Remove(company);
+            department.DepartmentName = dto.DepartmentName;
+
             _context.SaveChanges();
+
+            return department.Id;
         }
 
         // DELETE : delete department with id
