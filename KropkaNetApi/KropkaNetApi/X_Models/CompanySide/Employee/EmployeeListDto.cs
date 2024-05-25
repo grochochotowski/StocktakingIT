9b8aaea4efa41403e8708cd5863a29a4121db541
karichil <karina.chilkiewicz@gmail.com>
2024-05-25 14:30:28 +0200

    create EmployeeListDto

---
+++ KropkaNetApi/KropkaNetApi/X_Models/CompanySide/Employee/EmployeeListDto.cs
@@ -0,0 +1,16 @@
+﻿using KropkaNetApi.X_Models.CompanySide.Position;
+
+namespace KropkaNetApi.X_Models.CompanySide.Employee
+{
+    public class EmployeeListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PersonalNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Note { get; set; }
+        public virtual PositionDto? PositionName { get; set; }
+    }
+}
