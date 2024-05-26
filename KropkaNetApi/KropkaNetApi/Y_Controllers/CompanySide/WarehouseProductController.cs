f6276c6dc37a98f84346732a1dfcaff2dd668931
grochochotowski <g.m.rochowski@gmail.com>
2024-05-26 20:23:13 +0200

    setup warehouse product

---
+++ KropkaNetApi/KropkaNetApi/Y_Controllers/CompanySide/WarehouseProductController.cs
@@ -0,0 +1,21 @@
+﻿using KropkaNetApi.Y_Services.ClientSide;
+using KropkaNetApi.Y_Services.CompanySide;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KropkaNetApi.Y_Controllers.CompanySide
+{
+    [Route("api/kropkaNet/company")]
+    [ApiController]
+    //[Authorize(Roles = "Employee, Moderator, Admin")]
+    public class WarehouseProductController : ControllerBase
+    {
+        private readonly IWarehouseProductService _warehouseProductService;
+
+        public WarehouseProductController(IWarehouseProductService warehouseProductService)
+        {
+            _warehouseProductService = warehouseProductService;
+        }
+
+    }
+}
---
+++ KropkaNetApi/KropkaNetApi/Y_Services/CompanySide/WarehouseProductService.cs
@@ -0,0 +1,19 @@
+﻿using KropkaNetApi.X_Entities.Enum;
+using KropkaNetApi.X_Entities;
+using KropkaNetApi.X_Models.ClientSide.Company;
+using KropkaNetApi.X_Models.CompanySide.Product;
+
+namespace KropkaNetApi.Y_Services.CompanySide
+{
+    public interface IWarehouseProductService
+    {
+        ReturnResult<ProductListDto> GetFromWarehouse(int warehouseId, int page, string filter, string sortBy, SortDirection sortDireciton);
+        void AddProduct(int warehouseId, int productId, int quantity);
+        void RemoveProduct(int warehouseId, int productId, int quantity);
+        void EditQuantity(int warehouseId, int productId, int quantity);
+    }
+    public class WarehouseProductService
+    {
+
+    }
+}
