ee0056f3b1319f0775b47cc238987f55265a6ba7
grochochotowski <g.m.rochowski@gmail.com>
2024-06-01 10:05:35 +0200

    create new order box

--- KropkaNetFrontEnd/src/pages/OrderPages/OrderNew.jsx
+++ KropkaNetFrontEnd/src/pages/OrderPages/OrderNew.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function OrderNew() {
+
+	const [departments, setDepartments] = useState([
+		{
+			"id": 1,
+			"departmentName": "name1"
+		},
+		{
+			"id": 2,
+			"departmentName": "name2"
+		},
+		{
+			"id": 3,
+			"departmentName": "name3"
+		},
+		{
+			"id": 4,
+			"departmentName": "name4"
+		},
+		{
+			"id": 5,
+			"departmentName": "name5"
+		},
+	])
+	const [selectedDepartment, setSelectedDepartment] = useState('');
+	const [orderDate, setOrderDate] = useState('');
+
+	const handleDepartmentChange = (e) => {
+		setSelectedDepartment(e.target.value);
+	};
+
+	const handleDateChange = (e) => {
+		setOrderDate(e.target.value);
+	}; 
+
+	
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const dataToSend = {
+			dateOfOrderExecution: orderDate,
+			departmentId: selectedDepartment
+		};
+		console.log(dataToSend);
+	};
+
 	return (
 		<div className="outside-box">
 			<div className="content">
 				<h1>New order</h1>
+				<form>
+					<select name="department" id="department" onChange={handleDepartmentChange}>
+						{departments.map(department => (
+							<option key={department.id} value={department.id}>{department.departmentName}</option>
+						))}
+					</select>
+					<input 
+						type="datetime-local" 
+						id="order-date" 
+						name="order-date" 
+						value={orderDate} 
+						onChange={handleDateChange}
+					/>
+					<button type="submit" onClick={handleSubmit}>Create</button>
+				</form>
 			</div>
 		</div>
 	)
