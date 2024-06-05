37c5130cf09260efd132d40c22a01f6c921adf67
grochochotowski <g.m.rochowski@gmail.com>
2024-06-05 07:41:45 +0200

    update update company with users

--- KropkaNetFrontEnd/src/pages/CompanyPages/CompanyEdit.jsx
+++ KropkaNetFrontEnd/src/pages/CompanyPages/CompanyEdit.jsx
@@ -36,34 +36,43 @@ function CompanyEdit({ hideBox, updateData }) {
 	})
 
 	const [departments, setDepartments] = useState([
-		{
-			"id": 1,
-			"departmentName": "name1"
-		},
-		{
-			"id": 2,
-			"departmentName": "name2"
-		},
-		{
-			"id": 3,
-			"departmentName": "name3"
-		},
-		{
-			"id": 4,
-			"departmentName": "name4"
-		},
-		{
-			"id": 5,
-			"departmentName": "name5"
-		},
+		{ "id": 1, "departmentName": "name1" },
+		{ "id": 2, "departmentName": "name2" },
+		{ "id": 3, "departmentName": "name3" },
+		{ "id": 4, "departmentName": "name4" },
+		{ "id": 5, "departmentName": "name5" },
+		{ "id": 1, "departmentName": "name1" },
+		{ "id": 2, "departmentName": "name2" },
+		{ "id": 3, "departmentName": "name3" },
+		{ "id": 4, "departmentName": "name4" },
+		{ "id": 5, "departmentName": "name5" },
+		{ "id": 1, "departmentName": "name1" },
+		{ "id": 2, "departmentName": "name2" },
+		{ "id": 3, "departmentName": "name3" },
+		{ "id": 4, "departmentName": "name4" },
+		{ "id": 5, "departmentName": "name5" },
 	])
-
-	const [newDepartment, setNewDepartment] = useState("")
+	const [users, setUsers] = useState([
+		{ "id": 1, "name": "name1", "surname": "surname1" },
+		{ "id": 2, "name": "name2", "surname": "surname2" },
+		{ "id": 3, "name": "name3", "surname": "surname3" },
+		{ "id": 4, "name": "name4", "surname": "surname4" },
+		{ "id": 5, "name": "name5", "surname": "surname5" },
+		{ "id": 1, "name": "name1", "surname": "surname1" },
+		{ "id": 2, "name": "name2", "surname": "surname2" },
+		{ "id": 3, "name": "name3", "surname": "surname3" },
+		{ "id": 4, "name": "name4", "surname": "surname4" },
+		{ "id": 5, "name": "name5", "surname": "surname5" },
+		{ "id": 1, "name": "name1", "surname": "surname1" },
+		{ "id": 2, "name": "name2", "surname": "surname2" },
+		{ "id": 3, "name": "name3", "surname": "surname3" },
+		{ "id": 4, "name": "name4", "surname": "surname4" },
+		{ "id": 5, "name": "name5", "surname": "surname5" },
+	])
 	
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		//console.log(dataToSend);
-		hideBox();
 	};
 	const addDepartment = (e) => {
 		e.preventDefault();
@@ -79,60 +88,119 @@ function CompanyEdit({ hideBox, updateData }) {
         ))
     }
 
+	const [addUser, setAddUser] = useState("")
+	const [newDepartment, setNewDepartment] = useState("")
+	const handleUserChange = (e) => setAddUser(e.target.value);
+
 	function handleDelete(element) {
 		alert(`deleted ${element}`)
 	}
 
-    const [sorting, setSorting] = useState(["id", 0])
-	function sortTable(column) {
-        setSorting(prev => {
+    const [sortingUser, setSortingUser] = useState(["id", 0])
+	const [sortingDepartment, setSortingDepartment] = useState(["id", 0])
+	function sortTableUser(column) {
+        setSortingUser(prev => {
             if (prev[0] === column && prev[1] === 0) return [column, 1]
             return [column, 0]
         })
     }
-    function generateHeader() {
+	function sortTableDepartment(column) {
+        setSortingDepartment(prev => {
+            if (prev[0] === column && prev[1] === 0) return [column, 1]
+            return [column, 0]
+        })
+    }
+    function generateHeaderUser() {
         return (
             <thead>
                 <tr>
-                    <th className="u-thin" onClick={() => sortTable("id")}>
+                    <th className="u-thin" onClick={() => sortTableUser("id")}>
                         {
-                            sorting[0] == "id" &&
+                            sortingUser[0] == "id" &&
                             (
-                                sorting[1] === 0
+                                sortingUser[1] === 0
                                 ? <i className="fa-solid fa-arrow-down-a-z"></i>
                                 : <i className="fa-solid fa-arrow-up-a-z"></i>
                             )
                         }
                         ID
                     </th>
-                    <th className="wide" onClick={() => sortTable("departmentName")}>
+                    <th className="wide" onClick={() => sortTableUser("name")}>
                         {
-                            sorting[0] == "departmentName" &&
+                            sortingUser[0] == "name" &&
                             (
-                                sorting[1] === 0
+                                sortingUser[1] === 0
                                 ? <i className="fa-solid fa-arrow-down-a-z"></i>
                                 : <i className="fa-solid fa-arrow-up-a-z"></i>
                             )
                         }
-                        Department Name
+                        Name
                     </th>
-                    <th className="u-thin no-select">
-                        
+                    <th className="wide" onClick={() => sortTableUser("surname")}>
+                        {
+                            sortingUser[0] == "surname" &&
+                            (
+                                sortingUser[1] === 0
+                                ? <i className="fa-solid fa-arrow-down-a-z"></i>
+                                : <i className="fa-solid fa-arrow-up-a-z"></i>
+                            )
+                        }
+                        Surname
                     </th>
                 </tr>
             </thead>
         );
     }
-    function generateBody() {
+    function generateHeaderDepartment() {
+        return (
+            <thead>
+                <tr>
+                    <th className="u-thin" onClick={() => sortTableDepartment("id")}>
+                        {
+                            sortingDepartment[0] == "id" &&
+                            (
+                                sortingDepartment[1] === 0
+                                ? <i className="fa-solid fa-arrow-down-a-z"></i>
+                                : <i className="fa-solid fa-arrow-up-a-z"></i>
+                            )
+                        }
+                        ID
+                    </th>
+                    <th className="wide" onClick={() => sortTableDepartment("departmentName")}>
+                        {
+                            sortingDepartment[0] == "departmentName" &&
+                            (
+                                sortingDepartment[1] === 0
+                                ? <i className="fa-solid fa-arrow-down-a-z"></i>
+                                : <i className="fa-solid fa-arrow-up-a-z"></i>
+                            )
+                        }
+                        Name
+                    </th>
+                </tr>
+            </thead>
+        );
+    }
+    function generateBodyUser() {
+        return (
+            <tbody>
+                {users.map((user) => (
+                    <tr className="not-clickable" key={user.id} id={user.id}>
+                        <td className="u-thin">{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.surname}</td>
+                    </tr>
+                ))}
+            </tbody>
+        )
+    }
+    function generateBodyDepartment() {
         return (
             <tbody>
                 {departments.map((department) => (
                     <tr className="not-clickable" key={department.id} id={department.id}>
                         <td className="u-thin">{department.id}</td>
                         <td>{department.departmentName}</td>
-                        <td className="u-thin delete-item" onClick={() => handleDelete(department.id)}>
-							<i className="fa-solid fa-trash-can"></i>
-						</td>
                     </tr>
                 ))}
             </tbody>
@@ -141,8 +209,8 @@ function CompanyEdit({ hideBox, updateData }) {
 
 	return (
 		<div className="outside-box">
-			<div className="content big half">
-				<div className="half">
+			<div className="content big scroll">
+				<div className="top">
 					<h1>Edit company</h1>
 					<form>
 						<h4>Company data</h4>
@@ -248,22 +316,41 @@ function CompanyEdit({ hideBox, updateData }) {
 						<button type="submit" onClick={handleSubmit}>Update</button>
 					</form>
 				</div>
-				<div className="half">
-					<h1>Departments</h1>
-					<div className="list">
-						<table>
-							{ generateHeader() }
-							{ generateBody() }
-						</table>
-                	</div>
-					<div className="new-object">
-						<input
-                	        type="text"
-                	        id="newDepartment"
-                	        onChange={() => setNewDepartment(document.getElementById("newDepartment").value)}
-                	        value={newDepartment}
-                	    />
-						<button type="submit" onClick={addDepartment}>Add</button>
+				<div className="bottom divide">
+					<div className="half">
+						<h3>Users</h3>
+						<div className="list rows-5">
+							<table>
+								{ generateHeaderUser() }
+								{ generateBodyUser() }
+							</table>
+						</div>
+						<div className="new-object">
+							<select name="user" id="user" onChange={handleUserChange}>
+								{users.map(user => (
+									<option key={user.id} value={user.id}>{user.name} {user.surname}</option>
+								))}
+							</select>
+							<button type="submit" onClick={addDepartment}>Add</button>
+						</div>
+					</div>
+					<div className="half">
+						<h3>Departments</h3>
+						<div className="list rows-5">
+							<table>
+								{ generateHeaderDepartment() }
+								{ generateBodyDepartment() }
+							</table>
+						</div>
+						<div className="new-object">
+							<input
+								type="text"
+								id="newDepartment"
+								onChange={() => setNewDepartment(document.getElementById("newDepartment").value)}
+								value={newDepartment}
+							/>
+							<button type="submit" onClick={addDepartment}>Create</button>
+						</div>
 					</div>
 				</div>
 			</div>
