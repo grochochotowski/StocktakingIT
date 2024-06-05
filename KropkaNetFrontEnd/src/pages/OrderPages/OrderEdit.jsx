3ec7caaa838fb8ae61c14c62606e8f883d510d24
grochochotowski <g.m.rochowski@gmail.com>
2024-06-05 07:47:47 +0200

    fix date displaying in update order

--- KropkaNetFrontEnd/src/pages/OrderPages/OrderEdit.jsx
+++ KropkaNetFrontEnd/src/pages/OrderPages/OrderEdit.jsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react'
 
 function OrderNew({ hideBox, updateData }) {
 
-	const [orderDate, setOrderDate] = useState('');
+	const [orders, setOrders] = useState({
+		"id": 3,
+		"dateOfOrderExecution": "2024-05-27T20:48:41.712",
+		"state": 1,
+		"departmentName": "string2003",
+		"stocktakingId": null
+	})
+	const [orderDate, setOrderDate] = useState({
+		"dateOfOrderExecution": orders.dateOfOrderExecution
+	});
 
-	const handleDateChange = (e) => setOrderDate(e.target.value);
+	const handleDateChange = (e) => setOrderDate({"dateOfOrderExecution": e.target.value});
 	
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		const dataToSend = {
-			dateOfOrderExecution: orderDate
-		};
-		console.log(dataToSend);
+		console.log(orderDate);
 		hideBox();
 	};
 
@@ -22,13 +28,13 @@ function OrderNew({ hideBox, updateData }) {
 	return (
 		<div className="outside-box">
 			<div className="content">
-				<h1>New order</h1>
+				<h1>Update order</h1>
 				<form>
 					<input 
 						type="datetime-local" 
 						id="order-date" 
 						name="order-date" 
-						value={orderDate} 
+						value={orderDate.dateOfOrderExecution} 
 						onChange={handleDateChange}
 					/>
 					<button type="submit" onClick={handleSubmit}>Update</button>
