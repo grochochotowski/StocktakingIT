af353b2b9167a4891822c02b82398ce12b1db953
grochochotowski <g.m.rochowski@gmail.com>
2024-06-08 21:11:35 +0200

    dont add user to order if employee creates it

--- KropkaNetFrontEnd/src/pages/OrderPages/OrderNew.jsx
+++ KropkaNetFrontEnd/src/pages/OrderPages/OrderNew.jsx
@@ -41,7 +41,10 @@ function OrderNew({ hideBox, updateData }) {
 		};
 		
 		const token = await refreshToken();
-        const apiCall = `kropkaNet/order/create?userId=${state.personId}`;
+        const apiCall = `kropkaNet/order/create`
+		if (state.level == "user") {
+			apiCall += `?userId=${state.personId}`;
+		}
         try {
             const response = await axiosInstance.post(apiCall, dataToSend, {
                 headers: {
