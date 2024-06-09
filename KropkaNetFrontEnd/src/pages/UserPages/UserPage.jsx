3a28b781f68bfafbe6a07330f63a5e068f0b7a6e
grochochotowski <g.m.rochowski@gmail.com>
2024-06-09 17:52:27 +0200

    create main user page

--- KropkaNetFrontEnd/src/pages/UserPages/UserPage.jsx
+++ KropkaNetFrontEnd/src/pages/UserPages/UserPage.jsx
@@ -1,12 +1,276 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { GlobalStateContext } from '../../GlobalState';
+import { axiosInstance, refreshToken } from '../../api/axios';
+import { Link } from 'react-router-dom';
 
 import NavBarEmployee from '../../components/NavBarEmployee'
 
+import UserNew from './UserNew'
+import UserEdit from './UserEdit'
+import UserInfo from './UserInfo'
+
+import '../../styles/mainSubPage.css'
+import '../../styles/form.css'
+import '../../styles/list.css'
+import '../../styles/new.css'
+import '../../styles/info.css'
+
 function UserPage() {
+
+    const { state, setState } = useContext(GlobalStateContext);
+
+    const [newState, setNewState] = useState([0, 0])
+    const [sorting, setSorting] = useState(["id", 0])
+    const [filters, setFilters] = useState({ "filters" : "" })
+    const [selected, setSelected] = useState(0);
+    const [page, setPage] = useState(1);
+    const [result, setResult] = useState({})
+    const [box, setBox] = useState("");
+
+    async function fetchData() {
+        const token = await refreshToken();
+        let apiCall = `kropkaNet/user/getAll?` +
+            `${filters.filters && "filter=" + filters.filters + "&"}` +
+            `sortBy=${sorting[0]}&` +
+            `sortDirection=${sorting[1] == 0 ? "ASC" : "DESC"}&` +
+            `page=${page}`
+        try {
+            const response = await axiosInstance.get(apiCall, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setResult(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    useEffect(() => {
+      fetchData();
+    }, [sorting, page])
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+          if (event.target.closest(".outside-box") && !event.target.closest(".content")) setBox("");
+        }
+    
+        document.addEventListener("click", handleClickOutside);
+    
+        return () => {
+          document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
+    function sortTable(column) {
+        setSorting(prev => {
+            if (prev[0] === column && prev[1] === 0) return [column, 1]
+            return [column, 0]
+        })
+    }
+    function updateFilters(filter) {
+        setFilters(prev => ({
+            [filter] : document.getElementById(filter).value
+        }))
+    }
+    function filter() {
+        fetchData();
+    }
+
+    function generateHeader() {
+        return (
+            <thead>
+                <tr>
+                    <th className="thin" onClick={() => sortTable("id")}>
+                        {
+                            sorting[0] == "id" &&
+                            (
+                                sorting[1] === 0
+                                ? <i className="fa-solid fa-arrow-down-a-z"></i>
+                                : <i className="fa-solid fa-arrow-up-a-z"></i>
+                            )
+                        }
+                        ID
+                    </th>
+                    <th className="wide" onClick={() => sortTable("name")}>
+                        {
+                            sorting[0] == "name" &&
+                            (
+                                sorting[1] === 0
+                                ? <i className="fa-solid fa-arrow-down-a-z"></i>
+                                : <i className="fa-solid fa-arrow-up-a-z"></i>
+                            )
+                        }
+                        Name
+                    </th>
+                    <th className="wide" onClick={() => sortTable("surname")}>
+                        {
+                            sorting[0] == "surname" &&
+                            (
+                                sorting[1] === 0
+                                ? <i className="fa-solid fa-arrow-down-a-z"></i>
+                                : <i className="fa-solid fa-arrow-up-a-z"></i>
+                            )
+                        }
+                        Surname
+                    </th>
+                </tr>
+            </thead>
+        );
+    }
+    function generateBody() {
+        return (
+            <tbody>
+                {result.items && result.items.map((user) => (
+                    <tr className={user.id === selected ? "selected" : ""} key={user.id} id={user.id} onClick={() => setSelected(user.id)}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.surname}</td>
+                    </tr>
+                ))}
+            </tbody>
+        )
+    }
+    function generatePagination() {
+
+        const paginationItems = [];
+
+        if (result.length != 0) {
+
+            // Generate left arrow
+            if (page > 1) {
+                paginationItems.push(
+                    <li className="clickable" onClick={() => setPage(page - 1)} key={"arrow-left"}>
+                        <i className="fa-solid fa-caret-left"></i>
+                    </li>
+                )
+            }
+            else {
+                paginationItems.push(
+                    <li className="disable" key={"arrow-left"}>
+                        <i className="fa-solid fa-caret-left"></i>
+                    </li>
+                )
+            }
+
+            if (result.totalPages <= 7) {
+                for (let i = 1; i <= result.totalPages; i++) {
+                    if (i == page) {
+                    paginationItems.push(<li key={i} className="selected" onClick={() => setPage(i)}>{i}</li>);
+                    }
+                    else {
+                        paginationItems.push(<li key={i} className="clickable" onClick={() => setPage(i)}>{i}</li>);
+                    }
+                }
+            }
+            else {
+
+                if (page <= 4) {
+                    for (let i = 1; i <= 7; i++) {
+                        if (i == page) {
+                            paginationItems.push(<li key={i} className="selected" onClick={() => setPage(i)}>{i}</li>);
+                        }
+                        else {
+                            paginationItems.push(<li key={i} className="clickable" onClick={() => setPage(i)}>{i}</li>);
+                        }
+                    }
+                    paginationItems.push(<li key={"dots2"}>...</li>)
+                    paginationItems.push(<li key={result.totalPages} className="clickable" onClick={() => setPage(result.totalPages)}>{result.totalPages}</li>);
+                }
+                else if (result.totalPages - page < 5) {
+                    paginationItems.push(<li key={1} className="clickable" onClick={() => setPage(1)}>{1}</li>);
+                    paginationItems.push(<li key={"dots1"}>...</li>)
+                    for (let i = result.totalPages-6; i <= result.totalPages; i++) {
+                        if (i == page) {
+                            paginationItems.push(<li key={i} className="selected" onClick={() => setPage(i)}>{i}</li>);
+                        }
+                        else {
+                            paginationItems.push(<li key={i} className="clickable" onClick={() => setPage(i)}>{i}</li>);
+                        }
+                    }
+                }
+                else {
+                    paginationItems.push(<li key={1} className="clickable" onClick={() => setPage(1)}>{1}</li>);
+                    paginationItems.push(<li key={"dots1"}>...</li>)
+
+                    for (let i = page-2; i < page; i++) {
+                        paginationItems.push(<li key={i} className="clickable" onClick={() => setPage(i)}>{i}</li>);
+                    }
+
+                    paginationItems.push(<li key={page} className="selected" onClick={() => setPage(page)}>{page}</li>)
+
+                    for (let i = page+1; i <= page+2; i++) {
+                        paginationItems.push(<li key={i} className="clickable" onClick={() => setPage(i)}>{i}</li>);
+                    }
+                
+                    paginationItems.push(<li key={"dots2"}>...</li>)
+                    paginationItems.push(<li key={result.totalPages} className="clickable" onClick={() => setPage(result.totalPages)}>{result.totalPages}</li>);
+                }
+            }
+                
+            // Generate right arrow
+            if (page < result.totalPages) {
+                paginationItems.push(
+                    <li className="clickable" onClick={() => setPage(page + 1)} key={"arrow-right"}>
+                        <i className="fa-solid fa-caret-right"></i>
+                    </li>
+                )
+            }
+            else {
+                paginationItems.push(
+                    <li className="disable" key={"arrow-right"}>
+                        <i className="fa-solid fa-caret-right"></i>
+                    </li>
+                )
+            }
+            return paginationItems;
+        }
+    }
+
     return (
         <>
             <NavBarEmployee />
-            <div>UserPage</div>
+            <div className="container">
+                <div className="list">
+                    <div className="filter w-check">
+                        <input
+                            type="text"
+                            id="filters"
+                            onChange={() => updateFilters("filters")}
+                            value={filters.filters}
+                        />
+                        <button onClick={() => filter()}>Filter</button>
+                    </div>
+                    <table>
+                        { generateHeader() }
+                        { generateBody() }
+                    </table>
+                    <ul>
+                        { generatePagination() }
+                    </ul>
+                </div>
+                <div className="list-menu">
+                    <div className="current button objectOption" onClick={() => fetchData()}>
+                        <i className="fa-solid fa-list"></i>
+                        <p>Refresh data</p>
+                    </div>
+                    <div onClick={() => selected != 0 && setBox("info")} className={selected ? "button objectOption" : "disable button objectOption"}>
+                        <i className="fa-solid fa-info"></i>
+                        <p>Details</p>
+                    </div>
+                    <div onClick={() => selected != 0 && setBox("edit")} className={selected ? "button objectOption" : "disable button objectOption"}>
+                        <i className="fa-solid fa-pen-to-square"></i>
+                        <p>Edit</p>
+                    </div>
+                    <div onClick={() => setBox("new")} className="button objectOption">
+                        <i className="fa-solid fa-plus"></i>
+                        <p>New</p>
+                    </div>
+                </div>
+            </div>
+            { box && box == "new" && <UserNew hideBox={() => setBox("")} updateData={() => fetchData()}/> }
+            { box && box == "edit" && <UserEdit updateData={() => fetchData()} selected={selected}/> }
+            { box && box == "info" && <UserInfo selected={selected}/> }
         </>
     )
 }
