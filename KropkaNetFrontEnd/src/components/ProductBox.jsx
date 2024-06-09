bb4928cac4700f9f6697a70cee8b11a9aeddadb2
grochochotowski <g.m.rochowski@gmail.com>
2024-06-10 00:08:40 +0200

    display image of product

--- KropkaNetFrontEnd/src/components/ProductBox.jsx
+++ KropkaNetFrontEnd/src/components/ProductBox.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useContext } from 'react'
 import { axiosInstance, refreshToken } from '../api/axios';
 import { GlobalStateContext } from '../GlobalState';
 
-
 export default function ProductBox({warehouseId, product}) {
 
     const { state, setState } = useContext(GlobalStateContext);
 
     const [newQuantity, setNewQuantity] = useState(product.quantity)
+
+    const backendUrl = "https://localhost:44396";
 
     async function save() {
         let changeQuantity = newQuantity - product.quantity
@@ -38,8 +39,10 @@ export default function ProductBox({warehouseId, product}) {
 
     return (
         <div className="product-box">
-            {/*<img src={product.img ? meal.img : "https://i.pinimg.com/564x/ef/e8/d3/efe8d36db6281666a126189f05bfeff1.jpg"} alt="product-img" />*/}
-            <img src="https://i.pinimg.com/564x/ef/e8/d3/efe8d36db6281666a126189f05bfeff1.jpg" alt="product-img" />
+            <img
+                src={product.imgUrl ? `${backendUrl}${product.imgUrl}` : "https://i.pinimg.com/564x/ef/e8/d3/efe8d36db6281666a126189f05bfeff1.jpg"} 
+                alt="product-img" 
+            />
             <div className="product-info">
                 <h5>{product.name}</h5>
                 <p className="quantity">Quantity:&nbsp;
